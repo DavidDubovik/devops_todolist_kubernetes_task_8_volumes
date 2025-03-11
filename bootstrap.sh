8e170075d538cf65cd0ba3be83e575ac54f217c6
David <DavidDubovik19@gmail.com>
2025-03-11 14:40:42 +0200

    fix 5

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-kubectl apply -f pv.yml
-kubectl apply -f pvc.yml
-
+kubectl apply -f .infrastructure/pv.yml
+kubectl apply -f .infrastructure/pvc.yml
 kubectl apply -f .infrastructure/configmap.yml
 kubectl apply -f .infrastructure/secret.yml
 kubectl apply -f .infrastructure/cluster.yml
